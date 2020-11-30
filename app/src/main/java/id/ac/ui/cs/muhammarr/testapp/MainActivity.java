6c18f8bfb6e53e194878bb663f5e9b5eda7cb23f
Mohammad Ammar Ramadhan <mohammad.ammar61@ui.ac.id>
2020-11-30 21:57:05 +0700

    Upload lab-5

--- app/src/main/java/id/ac/ui/cs/muhammarr/testapp/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/muhammarr/testapp/MainActivity.java
@@ -1,25 +1,65 @@
 package id.ac.ui.cs.muhammarr.testapp;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-// Source: https://developer.android.com/training/basics/fragments/communicating.html
-public class MainActivity extends AppCompatActivity
-        implements ItemFragment.OnItemFragmentClickListener {
+import com.google.android.material.snackbar.Snackbar;
 
-    @Override
-    public void onAttachFragment(Fragment fragment) {
-        if (fragment instanceof ItemFragment) {
-            ItemFragment itemFragment = (ItemFragment) fragment;
-            itemFragment.setOnItemFragmentClickListener(this);
-        }
-    }
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.Manifest.permission.ACCESS_WIFI_STATE;
+import static android.Manifest.permission.CHANGE_WIFI_STATE;
+import static android.Manifest.permission.INTERNET;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WifiManager wifiManager;
+    private ArrayAdapter adapter;
+    private ArrayList<String> arrayList = new ArrayList<>();
+    private ListView listView;
+    private Button buttonScan;
+    private Button sendBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,24 +67,148 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.main_activity);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        buttonScan = findViewById(R.id.scan_btn);
+        buttonScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scanWifi();
+            }
+        });
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        //Create instance of your Fragment
-        Fragment fragment = new ItemFragment();
-        //Add Fragment instance to your Activity
-        fragmentTransaction.add(R.id.frame_layout, fragment);
-        fragmentTransaction.commit();
+        sendBtn = findViewById(R.id.send_list);
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                URL url = null;
+                try {
+                    url = new URL("https://041d8f90414553111af3ca9cf4e6446c.m.pipedream.net");
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+                HttpsURLConnection urlConnection = null;
+                try {
+                    urlConnection = (HttpsURLConnection) url.openConnection();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    urlConnection.setRequestMethod("POST");
+                    urlConnection.setRequestProperty("Content-Type", "application/json; utf-8");
+                    urlConnection.setDoOutput(true);
+                    JSONObject obj = new JSONObject();
+                    obj.put("message", Arrays.toString(arrayList.toArray()));
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+                        try (OutputStream os = urlConnection.getOutputStream()) {
+                            byte[] input = obj.toString().getBytes("utf-8");
+                            os.write(input, 0, input.length);
+                            os.flush();
+                        }
+                    }
+                } catch (IOException | JSONException e) {
+                    e.printStackTrace();
+                }
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+                    try (BufferedReader br = new BufferedReader(
+                            new InputStreamReader(urlConnection.getInputStream(), "utf-8"))) {
+                        StringBuilder response = new StringBuilder();
+                        String responseLine = null;
+                        while ((responseLine = br.readLine()) != null) {
+                            response.append(responseLine.trim());
+                        }
+                        System.out.println(response.toString());
+                        Toast.makeText(MainActivity.this, "Send Success!", Toast.LENGTH_LONG);
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }finally {
+                        urlConnection.disconnect();
+                    }
+                }
+            }
+        });
+
+        listView = findViewById(R.id.wifi_list);
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(adapter);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+    }
+
+    private void scanWifi() {
+        int coarseCheck = ContextCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION);
+        if(coarseCheck != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{ACCESS_COARSE_LOCATION}, 200);
+        } else {
+            if (!wifiManager.isWifiEnabled()) {
+                Toast.makeText(this, "WiFi is disabled ... We need to enable it", Toast.LENGTH_LONG).show();
+                wifiManager.setWifiEnabled(true);
+            }
+            arrayList.clear();
+
+            registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+            wifiManager.startScan();
+            Toast.makeText(this, "Scanning WiFi ...", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    BroadcastReceiver wifiReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            List<ScanResult> results = wifiManager.getScanResults();
+            unregisterReceiver(this);
+
+            for (ScanResult scanResult : results) {
+                arrayList.add(scanResult.SSID + " - " + scanResult.capabilities);
+                adapter.notifyDataSetChanged();
+            }
+        };
+    };
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(MainActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
     }
 
     @Override
-    public void onItemFragmentClicked(Bundle bundle) {
-        DetailFragment detail = new DetailFragment();
-        detail.setArguments(bundle);
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 200:
+                if (grantResults.length > 0) {
 
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.frame_layout, detail);
-        transaction.addToBackStack(null);
-        transaction.commit();
+                    boolean accepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+
+                    if (accepted) {
+                        Snackbar.make(findViewById(R.id.scan_btn), "Permission Granted, Now you can scan wifi.", Snackbar.LENGTH_LONG).show();
+                    } else {
+                        Snackbar.make(findViewById(R.id.scan_btn), "Permission Denied, You cannot scan wifi.", Snackbar.LENGTH_LONG).show();
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            if (shouldShowRequestPermissionRationale(ACCESS_FINE_LOCATION)) {
+                                showMessageOKCancel("You need to allow access to both the permissions",
+                                        new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                    requestPermissions(new String[]{ACCESS_COARSE_LOCATION}, 200);
+                                                }
+                                            }
+                                        });
+                                return;
+                            }
+                        }
+
+                    }
+                }
+                break;
+        }
     }
 }
